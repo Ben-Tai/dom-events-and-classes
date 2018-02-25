30f8bb9f49312c9c31f460acb0f7b2b5fd99abd4
ben-tai <jamin.tairea@gmail.com>
2018-02-26 00:08:58 +1300

    Added updatecount function

--- game.js
+++ game.js
@@ -42,6 +42,19 @@ function updateCounts () {
   }
   
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+  var count = document.getElementsByClassName('board')[0].children
+  for (var i = 0; i < count.length; i++){
+    if (count[i].classList.contains('blue')){
+      totals.blue++
+    }
+    if (count[i].classList.contains('green')){
+      totals.green++
+    }
+    if (count[i].classList.contains('invisible')){
+      totals.invisible++
+    }
+    
+  }
 
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
